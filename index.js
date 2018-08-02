d216e3437676f760be520731842931895b5bfc7c
Christa Gabert <gabert.cm@gmail.com>
2018-08-02 16:24:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,7 +16,8 @@ function logWhisper(string) {
 
 function sayHiToGrandma(string) {
   if (string === string.toUpperCase()) {
-    return 'YES INDEED!' {
+    return 'YES INDEED!'
+  }
       expect('I can\'t hear you!'(.toEqual 'YES INDEED!'))
     } else if (string === 'I love you, Grandma.') {
       return 'I love you, too.'
